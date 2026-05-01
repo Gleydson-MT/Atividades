1e308e29f3ff74965e0cb1b7a90cb4e2e2a2b9f1
Gleydson-MT <jg16031996@gmail.com>
2026-04-30 21:21:46 -0300

    op

---
+++ q1.py
@@ -0,0 +1,9 @@
+#1. Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+while True:
+    nota = int(input("Digite uma nota entre 0 e 10: "))
+    if nota < 0 or nota > 10:
+        print("DIGITE UMA NOTA VÁLIDA")
+        continue
+    else:
+        print(f"Agradeçemos seu feedback!")
+        break
