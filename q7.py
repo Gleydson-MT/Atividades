e5888b0b90f82e843d588e99432be5d6c3b01695
Gleydson-MT <275184323+Gleydson-MT@users.noreply.github.com>
2026-05-07 21:14:46 -0300

    quinta

--- q7.py
+++ q7.py
@@ -1,1 +1,16 @@
-#46. Em uma competição de salto em distância cada atleta tem direito a cinco saltos. No final da série de saltos de cada atleta, o melhor e o pior resultados são eliminados. O seu resultado fica sendo a média dos três valores restantes. Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas pelo atleta em seus saltos e depois informe a média dos saltos conforme a descrição acima informada (retirar o melhor e o pior salto e depois calcular a média). Faça uso de uma lista para armazenar os saltos. Os saltos são informados na ordem da execução, portanto não são ordenados.
+#28. Faça um programa que calcule o valor total investido por um colecionador em sua coleção de CDs e o valor médio gasto em cada um deles. O usuário deverá informar a quantidade de CDs e o valor para cada um
+
+qtd_cds = int(input("Digite quantos cds foram adqueridos: "))
+valor_cds = 0
+
+for i in range(1):
+    while True:
+        if qtd_cds == 0:
+            print("DIGITE UM VALOR VÁLIDO!")
+        else:
+            valor_cada_cd = float(input("Digite o valor pago nos CDs: "))
+            valor_cds += valor_cada_cd 
+            break
+media = valor_cds/qtd_cds
+
+print(f"Valor pago por cd em média foi de {media}")
